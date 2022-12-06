174d7f11b416c5a582e56531e60b808ff53b4289
DIEGOA363 <dalba@uw.edu>
2022-12-05 16:07:43 -0800

    221130

--- troubleshoot.py
+++ troubleshoot.py
@@ -3,25 +3,47 @@ from whispr import *
 """
 For user to change according to run
 """
-
-# gene expression. 3ul rxns, 75% txtl
-rxn_vol = 3
-source_plate_type = '384PP_AQ_BP' 
-
-sp_plasmids_file = 'source_plates/221024_arpae_plasmids.xlsx'
-sp_plasmids = pd.read_excel(sp_plasmids_file, index_col = 0)
-sp_plasmids = sp_plasmids[~sp_plasmids['Well'].isna()]
-
-filename = '221024_arpae_genex.csv'
+filename = '221130_arpae_genex.csv'
 layout_genex_file = 'plate_layouts/'+filename 
 layout_genex = pd.read_csv(layout_genex_file, index_col = 0, dtype = str)
 
-mt_genex_file = 'mixing_tables/221024_arpae_plasmids.csv'
-mt_genex = pd.read_csv(mt_genex_file, index_col = 0, dtype = str).fillna(0)
 
-checkInputs(sp_plasmids, mt_genex,source_plate_type)
+# biosynthesis. 25ul rxns, 2.5ul of diluted txtl
 
-vol_table_df = generateVolumeTable(mt_genex, sp_plasmids, rxn_vol = 0.25*rxn_vol, total_vol = rxn_vol, fill_with='water', multiRpW = False)
+# buffer source plate
+sp_buffers_file = 'source_plates/221130_arpae_buffers.xlsx'
+sp_buffers= pd.read_excel(sp_buffers_file, index_col = 0)
+sp_buffers = sp_buffers[~sp_buffers['Well'].isna()]
 
-#specify rxn_vol (default = 2.5) and total_vol (default = 10) if you'd like to change the volume of each individual replicate or the total reaction volume
-protocol_genex_df = writeProtocol(source_plate_type, vol_table_df, layout_genex,sp_plasmids, update_source_vol='source_plates/221018_arpae_plasmids_updated.xlsx')
+# HEPES reservoir (actually same plate as Tris)
+sp_hepes = pd.DataFrame(columns=sp_buffers.columns)
+sp_hepes = sp_hepes.append({'Label':'HEPES', 'Well':'A2,A3', 'Concentration':'', 'Volume':'2000,2000'}, ignore_index = True)
+
+# gene expression source plate (diluted w/ Tris)
+sp_genex = sp_from_layout(layout_genex, 60)
+
+# group source plates
+sp_types = ['384PP_AQ_BP','6RES_AQ_BP2','384PP_AQ_BP'] # triple check this
+sps = [sp_buffers,sp_hepes,sp_genex]
+
+# get mixing table
+mt_biosyn_file = 'mixing_tables/221130_arpae_buffers.csv'
+mt_biosyn = pd.read_csv(mt_biosyn_file, index_col = 0, dtype = str).fillna(0)
+
+# check formats
+checkInputs(sps,mt_biosyn,sp_types)
+
+# layouts for destination plate(s)
+filename = '221130_arpae_biosyn.csv'
+layout_biosyn1_file = 'plate_layouts/'+filename 
+layout_biosyn1 = pd.read_csv(layout_biosyn1_file, index_col = 0, dtype = str)
+# filename = '221018_arpae_biosyn_neg.csv'
+# layout_biosyn2_file = 'plate_layouts/'+filename 
+# layout_biosyn2 = pd.read_csv(layout_biosyn2_file, index_col = 0, dtype = str)
+# layouts = [layout_biosyn1,layout_biosyn2]
+
+vol_table_df = generateVolumeTable(mt_biosyn, sps, rxn_vol = 22.5, total_vol = 25, fill_with='HEPES')
+
+protocol_biosyn_dfs = writeProtocol(sp_types, vol_table_df, layout_biosyn1,sps, update_source_vol='source_plates/221130_arpae_combined_sp_updated.xlsx')
+
+#protocol_biosyn_df.to_csv('protocols/221018_arpae_biosyn.csv',index = False)
